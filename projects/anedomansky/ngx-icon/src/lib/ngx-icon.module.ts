155251c42083e9ec3e06aee6b533f1453798c950
anedomansky <nedomanskya@outlook.com>
2023-12-19 18:47:53 +0100

    Removed unnecessary files

--- projects/anedomansky/ngx-icon/src/lib/ngx-icon.module.ts
+++ projects/anedomansky/ngx-icon/src/lib/ngx-icon.module.ts
@@ -1,39 +1,9 @@
-import { ModuleWithProviders, NgModule } from '@angular/core';
-
-import {
-  getDefaultNgxIconConfig,
-  NgxIconConfig,
-  NgxIconConfigToken,
-} from './config/ngx-icon-config';
-import { SampleServiceToken } from './core/services/sample/sample.config';
-import { SampleService } from './core/services/sample/sample.service';
-import { SampleComponent } from './sample/sample.component';
+import { NgModule } from '@angular/core';
 
 @NgModule({
   declarations: [],
-  imports: [SampleComponent],
-  providers: [
-    {
-      provide: SampleServiceToken,
-      useClass: SampleService,
-    },
-    {
-      provide: NgxIconConfigToken,
-      useValue: getDefaultNgxIconConfig(),
-    },
-  ],
-  exports: [SampleComponent],
+  imports: [],
+  providers: [],
+  exports: [],
 })
-export class NgxIconModule {
-  static forRoot(config: NgxIconConfig): ModuleWithProviders<NgxIconModule> {
-    return {
-      ngModule: NgxIconModule,
-      providers: [
-        {
-          provide: NgxIconConfigToken,
-          useValue: config,
-        },
-      ],
-    };
-  }
-}
+export class NgxIconModule {}
--- projects/ngx-icon-app/src/main.ts
+++ projects/ngx-icon-app/src/main.ts
@@ -1,23 +1,9 @@
+import { NgxIconModule } from '@anedomansky/ngx-icon';
 import { importProvidersFrom } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
-import { provideRouter } from '@angular/router';
-import { SampleServiceToken, NgxIconModule } from '@anedomansky/ngx-icon';
 
 import { AppComponent } from './app/app.component';
-import { APP_ROUTES } from './app/app.routes';
-import { AppSampleService } from './app/sample/sample.service';
 
 bootstrapApplication(AppComponent, {
-  providers: [
-    importProvidersFrom(
-      NgxIconModule.forRoot({
-        appTitle: 'App',
-      }),
-    ),
-    provideRouter(APP_ROUTES),
-    {
-      provide: SampleServiceToken,
-      useClass: AppSampleService,
-    },
-  ],
+  providers: [importProvidersFrom(NgxIconModule)],
 });
