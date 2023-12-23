33aff6b04279430b59148982542a09a7ab35343c
anedomansky <nedomanskya@outlook.com>
2023-12-23 19:55:15 +0100

    Added IconService unit tests

--- projects/anedomansky/ngx-icon/src/lib/services/icon.service.spec.ts
+++ projects/anedomansky/ngx-icon/src/lib/services/icon.service.spec.ts
@@ -1,19 +1,62 @@
-import { HttpClientTestingModule } from '@angular/common/http/testing';
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
 
 import { IconService } from './icon.service';
 
 describe('IconService', () => {
   let service: IconService;
+  let httpTestingController: HttpTestingController;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [HttpClientTestingModule],
     });
     service = TestBed.inject(IconService);
+    httpTestingController = TestBed.inject(HttpTestingController);
+  });
+
+  afterEach(() => {
+    // After every test, assert that there are no more pending requests.
+    httpTestingController.verify();
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should return SVG', () => {
+    service.addIcon('test', 'assets/');
+    service.getIcon('test').subscribe((svg) => expect(svg).toBe('<svg></svg>'));
+
+    const req = httpTestingController.expectOne('assets/test.svg');
+    expect(req.request.method).toEqual('GET');
+
+    req.flush('<svg></svg>');
+  });
+
+  it('should throw error if non existent SVG is retrieved', (done) => {
+    service.addIcon('test', 'assets/');
+    service.getIcon('notExisting').subscribe({ error: (err) => {
+      expect(err.message).toBe('Unable to find icon with the name "notExisting"');
+      done();
+    }});
+
+    httpTestingController.expectNone('assets/notExisting.svg');
+  });
+
+  it('should throw error if SVG file does not contain a <svg> element', (done) => {
+    service.addIcon('test', 'assets/');
+    service.getIcon('test').subscribe({ error: (err) => {
+      expect(err.message).toBe('<svg> not found');
+      done();
+    }});
+
+    const req = httpTestingController.expectOne('assets/test.svg');
+    expect(req.request.method).toEqual('GET');
+
+    req.flush('<div></div>');
   });
 });
